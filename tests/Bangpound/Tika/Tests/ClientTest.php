4c6fbce5b63e809e88177a3e7cde98c3081e3545
Benjamin Doherty <bjd@bangpound.org>
2013-09-22 10:52:48 -0500

    create tests.

---
+++ tests/Bangpound/Tika/Tests/ClientTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Bangpound\Tika\Tests;
+
+use Bangpound\Tika\Client;
+use Guzzle\Http\Message\Response;
+
+/**
+ * @group server
+ * @covers Bangpound\Tika\Client
+ */
+class ClientTest extends \Guzzle\Tests\GuzzleTestCase
+{
+    public function testVersion()
+    {
+        $client = $this->getServiceBuilder()->get('test.mock');
+        $this->setMockResponse($client, array('version.txt'));
+
+        /** @var \Guzzle\Http\Message\Response $response */
+        $response = $client->version();
+        $version = $response->getBody(TRUE);
+        $this->assertRegExp('/^Apache Tika (\d+\\.)?(\d+\\.)?(\\*|\d+)$/', $version);
+    }
+
+    public function testGreeting()
+    {
+        $client = $this->getServiceBuilder()->get('test.mock');
+        $this->setMockResponse($client, array('greeting.txt'));
+
+        /** @var \Guzzle\Http\Message\Response $response */
+        $response = $client->greeting();
+        $greeting = $response->getBody(TRUE);
+        $this->assertContains('This is Tika Server. Please PUT', $greeting);
+    }
+
+    public function testTika()
+    {
+        $client = $this->getServiceBuilder()->get('test.mock');
+        $this->setMockResponse($client, array(
+            'tika.txt',
+        ));
+
+        $file = __DIR__ .'/../../../../' . $_SERVER['TIKA_SRC_PATH'] . DIRECTORY_SEPARATOR .'testPDF.pdf';
+
+        /** @var \Bangpound\Tika\TikaResponse $response */
+        $response = $client->tika(array('file' => fopen($file, 'r')));
+
+        $this->assertEquals('application/pdf', $response->metadata('Content-Type'));
+        $this->assertEquals('Bertrand Delacrétaz', $response->metadata('creator'));
+        $this->assertEquals('Bertrand Delacrétaz', $response->metadata('Author'));
+        $this->assertEquals('Firefox', $response->metadata('xmp:CreatorTool'));
+        $this->assertEquals('Apache Tika - Apache Tika', $response->metadata('dc:title'));
+
+        $this->assertEquals('2007-09-15T09:02:31Z', $response->metadata('date'));
+        $this->assertEquals('2007-09-15T09:02:31Z', $response->metadata('modified'));
+
+        $content = $response->getBody(TRUE);
+        $this->assertContains('Apache Tika', $content);
+        $this->assertContains('Tika - Content Analysis Toolkit', $content);
+        $this->assertContains('incubator', $content);
+        $this->assertContains('Apache Software Foundation', $content);
+        $this->assertNotContains('ToolkitApache', $content, 'should have word boundary after headline');
+        $this->assertNotContains('libraries.Apache', $content, 'should have word boundary between paragraphs');
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+use Guzzle\Tests\GuzzleTestCase;
+use Guzzle\Service\Builder\ServiceBuilder;
+
+error_reporting(E_ALL | E_STRICT);
+
+// Ensure that composer has installed all dependencies
+if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
+    die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
+        . "See http://getcomposer.org for help with installing composer\n");
+}
+
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+// Include the composer autoloader
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Set mock directory for Tika command responses.
+GuzzleTestCase::setMockBasePath(__DIR__. DIRECTORY_SEPARATOR . 'mock');
+
+// Add the services file to the default service builder
+GuzzleTestCase::setServiceBuilder(ServiceBuilder::factory(array(
+    'base' => array(
+        'class' => 'Bangpound\\Tika\\Client',
+    ),
+    'test.tika' => array(
+        'extends' => 'base',
+        'params' => array(
+            'base_url' => $_SERVER['TIKA_URL'],
+        ),
+    ),
+    'test.mock' => array(
+        'extends' => 'base',
+        'params' => array(),
+    ),
+)));
