9be089062de5f969a87cc3baaa42923fbf2ea931
Benjamin Doherty <bjd@bangpound.org>
2014-04-10 13:30:02 -0500

    don't assume all tika responses have pages.

--- src/Bangpound/Tika/TikaResponse.php
+++ src/Bangpound/Tika/TikaResponse.php
@@ -9,17 +9,12 @@ use Guzzle\Service\Command\ResponseClassInterface;
  * Class TikaResponse
  * @package Bangpound\Tika
  */
-class TikaResponse extends Metadata implements ResponseClassInterface, \ArrayAccess, \Countable, \Iterator, \Serializable
+class TikaResponse extends Metadata implements ResponseClassInterface, \Serializable
 {
     /**
      * @var \SimpleXMLElement
      */
     private $xml;
-
-    /**
-     * @var int
-     */
-    private $position = 1;
 
     /**
      * @param OperationCommand $command
@@ -42,7 +37,6 @@ class TikaResponse extends Metadata implements ResponseClassInterface, \ArrayAcc
     {
         $this->xml = $xml;
         $this->metadata = array();
-        $this->position = 1;
         /** @var \SimpleXMLElement $child */
         foreach ($this->getHead()->children()->meta as $child) {
             $key = (string) $child['name'];
@@ -73,130 +67,6 @@ class TikaResponse extends Metadata implements ResponseClassInterface, \ArrayAcc
     public function getBody($asString = false)
     {
         return $asString ? $this->xml->body->asXML() : $this->xml->body;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetExists($offset)
-    {
-        $this->registerXPathNamespaces();
-        $values = $this->xml->xpath('/default:html/default:body/default:div[@class="page"]['. $offset .']');
-
-        return !empty($values);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetGet($offset)
-    {
-        $this->registerXPathNamespaces();
-        $values = $this->xml->xpath('/default:html/default:body/default:div[@class="page"]['. $offset .']');
-
-        return array_pop($values);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetSet($offset, $value)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetUnset($offset)
-    {
-    }
-
-    private function registerXPathNamespaces()
-    {
-        $namespaces = $this->xml->getNamespaces();
-        foreach ($namespaces as $prefix => $ns) {
-            if ($prefix === '') {
-                $this->xml->registerXPathNamespace('default', $ns);
-            } else {
-                $this->xml->registerXPathNamespace($prefix, $ns);
-            }
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function count()
-    {
-        $this->registerXPathNamespaces();
-
-        return count($this->xml->xpath('/default:html/default:body/default:div[@class="page"]'));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function current()
-    {
-        return $this[$this->position];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function next()
-    {
-        ++$this->position;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function key()
-    {
-        return $this->position;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function valid()
-    {
-        return isset($this[$this->position]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rewind()
-    {
-        $this->position = 1;
-    }
-
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        $output = '';
-        foreach ($this as $page) {
-            $output .= $page->asXML();
-        }
-
-        return $output;
-    }
-
-    /**
-     * @return array
-     */
-    public function toArray()
-    {
-        $output = array();
-        foreach ($this as $page) {
-            $output[] = $page->asXML();
-        }
-
-        return $output;
     }
 
     /**
